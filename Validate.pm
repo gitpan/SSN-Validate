3079289934ce3323f1806c0af073fc18592a9a2e
Kevin Meltzer <kmeltz@cpan.org>
2003-08-25 11:53:10 +0000

    Import of KMELTZ/SSN-Validate-0.02 from CPAN.
    gitpan-cpan-distribution: SSN-Validate
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         KMELTZ/SSN-Validate-0.02.tar.gz
    gitpan-cpan-author:       KMELTZ
    gitpan-cpan-maturity:     released

--- Validate.pm
+++ Validate.pm
@@ -4,16 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-#require Exporter;
-#use AutoLoader qw(AUTOLOAD);
-
-#our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 # Preloaded methods go here.
 
